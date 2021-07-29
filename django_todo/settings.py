6da6793cb50e68f18b53399c330c6be0dc1f1187
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-07-29 07:43:33 +0000

    Set up environment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 
-from pathlib import Path
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
+
+development = os.environ.get('DEVELOPMENT', False)
+# This means that if there's an environment variable called DEVELOPMENT in the environment.
+# This variable will be set to its value. And otherwise, it'll be false.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

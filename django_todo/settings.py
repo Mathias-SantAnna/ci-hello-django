17c49bc3ca1a28cdc6ae333d8c6efb93d127ab40
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-07-29 08:47:14 +0000

    Going back in time

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mtalcs-django-todo-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mtalcs-django-todo-app.herokuapp.com', 'localhost']
 
 
 # Application definition
